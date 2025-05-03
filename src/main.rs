0e535553d5d2502579695de27fb79249f041c9a2
brynhild <kiritsukiyena@gmail.com>
2025-05-03 20:50:27 +0900

    test one use

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use solana_sdk::signature::{Keypair, Signer};
+use bs58;
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::Write;
+
+
+#[derive(Serialize, Deserialize)]
+struct Wallet {
+    public_key: String,
+    secret_key: String, // Base58 encoded
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    // Generate a new keypair
+    let keypair = Keypair::new();
+
+    let public_key = keypair.pubkey().to_string();
+    let secret_key = bs58:: encode(keypair.to_bytes()).into_string(); 
+
+    let wallet = Wallet {
+        public_key,
+        secret_key,
+    };
+
+    //save the wallet to a file
+    let json = serde_json :: to_string_pretty(&wallet).expect("serialization failed");
+    let mut file = File :: create("wallet.json").expect("could not create file");
+    file.write_all(json.as_bytes()).expect("could not write to file");
+
+
+    println!("you have generated a new keypair");
+    println!("Public Key: {}", wallet.public_key);
+    println!("Secret Key: {}", wallet.secret_key);
+    println!("Wallet saved to wallet.json");
+    println!("You can use this wallet to interact with the Solana blockchain.");
+    println!("Keep your secret key safe and never share it with anyone.");
+
+
+
 }
